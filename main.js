41a82e0088a1d010fa261791d9448e6eafe04378
Sean Chavez <realseanchavez@gmail.com>
2019-05-10 21:18:17 -0700

    jquery working

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 const page = $('#page-manager')  //document.querySelector('#page-manager');
-page.parent().remove(page)  //page.parentNode.removeChild(page);
+page.remove()  //page.parentNode.removeChild(page);
 //alert("Hello from your Chrome extension!")
 
