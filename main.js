066841392e352fccfa6c3f1916e4f759ce191822
Sean Chavez <realseanchavez@gmail.com>
2019-05-11 13:26:48 -0700

    add img wip

--- main.js
+++ main.js
@@ -1,4 +1,21 @@
+console.log('FUCK')
 const page = $('#page-manager')  //document.querySelector('#page-manager');
+//const pageParent = page.parent()
+const content = $('#content')
 page.remove()  //page.parentNode.removeChild(page);
+const imgURL = chrome.runtime.getURL("aishath-naj-662589-unsplash.jpg")
+const newPageManager = document.createElement("ytd-page-manager")
+newPageManager.id = "bullshit"
+const img = document.createElement("img")
+img.src = imgURL
+newPageManager.appendChild(img)
+content.appendChild(div)
+//content.appendChild(img)
+// const image = $('<img />')
+//   .attr('src', imgURL)
+//   .attr('alt', "Person standing at the top of a mountain in the distance.")
+//   content.prepend(image)
+//pageParent.prepend(image)
+//pageParent.prepend('<img src="aishath-naj-662589-unsplash.jpg">')
 //alert("Hello from your Chrome extension!")
 
