af9600cc4780050d3176dcdc4d02e14beac56d38
Sean Chavez <realseanchavez@gmail.com>
2019-05-11 16:44:03 -0700

    image working

--- main.js
+++ main.js
@@ -6,13 +6,16 @@ const page = document.querySelector('#page-manager');
 const content = document.querySelector('#content')
 //page.remove()  
 page.parentNode.removeChild(page);
-const imgURL = chrome.runtime.getURL("aishath-naj-662589-unsplash.jpg")
-const newPageManager = document.createElement("ytd-page-manager")
-newPageManager.id = "bullshit"
+const imgURL = chrome.runtime.getURL("images/aishath-naj-662589-unsplash.jpg")
+//const newPageManager = document.createElement("ytd-page-manager")
+const div = document.createElement("div")
+//newPageManager.id = "bullshit"
+div.id = "fuck"
 const img = document.createElement("img")
 img.src = imgURL
-newPageManager.appendChild(img)
-content.appendChild(newPageManager)
+div.appendChild(img)
+//newPageManager.appendChild(div)
+content.appendChild(div)
 //content.appendChild(img)
 // const image = $('<img />')
 //   .attr('src', imgURL)
