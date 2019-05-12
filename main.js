5abcb80c08784093caffc3a737c2e45a0cd3d60e
Sean Chavez <realseanchavez@gmail.com>
2019-05-12 12:28:14 -0700

    implement fetch random photo functionality

--- main.js
+++ main.js
@@ -1,28 +1,34 @@
-console.log('FUCK')
-//const page = $('#page-manager')  
-const page = document.querySelector('#page-manager');
+console.log("FUCK");
+//const page = $('#page-manager')
+const page = document.querySelector("#page-manager");
 //const pageParent = page.parent()
 //const content = $('#content')
-const content = document.querySelector('#content')
-//page.remove()  
+const content = document.querySelector("#content");
+//page.remove()
 page.parentNode.removeChild(page);
-const imgURL = chrome.runtime.getURL("images/aishath-naj-662589-unsplash.jpg")
+//const imgURL = chrome.runtime.getURL("images/aishath-naj-662589-unsplash.jpg")
+const image = fetch("https://picsum.photos/list")
+  .then(response => response.json())
+  .then(photoArr => photoArr[Math.floor(Math.random() * photoArr.length)]);
+const imgURL = chrome.runtime.getURL(
+  "'https://unsplash.it/1200/800?image=' + image.id"
+);
 //const newPageManager = document.createElement("ytd-page-manager")
-const div = document.createElement("div")
+const div = document.createElement("div");
 //newPageManager.id = "bullshit"
-div.id = "fuck"
-const message = document.createElement("h1")
-message.className = "fancy-text"
-message.innerHTML = "Keep pushing... you'll get there."
-message.style.margin = "60px 0px 0px 75px" 
-div.appendChild(message)
-const img = document.createElement("img")
-img.src = imgURL
-img.height = 840
-img.width = 1260
-div.appendChild(img)
+div.id = "fuck";
+const message = document.createElement("h1");
+message.className = "fancy-text";
+message.innerHTML = "Keep pushing... you'll get there.";
+message.style.margin = "60px 0px 0px 75px";
+div.appendChild(message);
+const img = document.createElement("img");
+img.src = imgURL;
+img.height = 840;
+img.width = 1260;
+div.appendChild(img);
 //newPageManager.appendChild(div)
-content.appendChild(div)
+content.appendChild(div);
 //content.appendChild(img)
 // const image = $('<img />')
 //   .attr('src', imgURL)
@@ -31,4 +37,3 @@ content.appendChild(div)
 //pageParent.prepend(image)
 //pageParent.prepend('<img src="aishath-naj-662589-unsplash.jpg">')
 //alert("Hello from your Chrome extension!")
-
